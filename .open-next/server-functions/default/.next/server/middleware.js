f9f51c6117d51d6e504171cba05a966da4676f90
RaihanMufthahul223 <raihanmufthahul2005@gmail.com>
2026-09-24 13:16:59 +0700

    fix: cloudflare pages edge runtime + middleware compat + remove .open-next from git

--- .open-next/server-functions/default/.next/server/middleware.js
+++
@@ -1,6 +0,0 @@
-var R=require("./chunks/[turbopack]_runtime.js")("server/middleware.js")
-R.c("server/chunks/[root-of-the-server]__1avpecw._.js")
-R.c("server/chunks/[root-of-the-server]__1ugmq9q._.js")
-R.c("server/chunks/node_modules_next_dist_0ud7_je._.js")
-R.m(489997)
-module.exports=R.m(489997).exports
